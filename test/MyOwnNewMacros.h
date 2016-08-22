106b7b938ac9f4b80138291a12e11bf7d4e07f7d
Sid S. Ryu <sid@sidryu.com>
2016-08-22 21:09:17 +0900

    Fix cpputest conflict STL compile error
    std::set 컴파일 에러가 cpputest를 잘못 포함해 나는 문제라 수정.
    문서에 잘 나와있었음. https://cpputest.github.io/manual.html#memory_leak_detection

---
+++ test/MyOwnNewMacros.h
@@ -0,0 +1,10 @@
+// CppUTest: Confilicts with operator new macros (STL!)
+// https://cpputest.github.io/manual.html#memory_leak_detection
+
+// include stl
+#include <initializer_list>
+#include <set>
+#include <array>
+
+// cpputest MemoryLeakDetectorNewMacros
+#include "CppUTest/MemoryLeakDetectorNewMacros.h"
